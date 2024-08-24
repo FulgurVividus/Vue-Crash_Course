a3f48f278c8af9383c2429853811486d905b8f70
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-24 17:59:28 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  data() {
+    return {
+      name: "John Doe",
+      status: "active",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+</template>
