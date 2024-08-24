e7e9ebae15dd99bbd9274e43c16587d6e9beaf3e
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-24 19:15:42 +0500

    chore: v-for derictive & looping

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ export default {
     return {
       name: "John Doe",
       status: "active",
+      tasks: ["Task 1", "Task 2", "Task 3"],
     };
   },
 };
@@ -14,4 +15,9 @@ export default {
   <p v-if="status === 'active'">User is active</p>
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else="status">User is not active</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
 </template>
