862c08530071bddec750e4e0020ffb2190abd8e6
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-24 19:53:23 +0500

    chore: Composition API Short Form

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,18 @@
-<script>
+<script setup>
 import { ref } from "vue";
 
-export default {
-  setup() {
-    const name = ref("John Doe");
-    const status = ref("active");
-    const tasks = ref(["Task 1", "Task 2", "Task 3"]);
+const name = ref("John Doe");
+const status = ref("active");
+const tasks = ref(["Task 1", "Task 2", "Task 3"]);
 
-    const toggleStatus = () => {
-      if (status.value === "active") {
-        status.value = "pending";
-      } else if (status.value === "pending") {
-        status.value = "active";
-      } else {
-        status.value = "active";
-      }
-    };
-
-    return { name, status, tasks, toggleStatus };
-  },
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+  } else if (status.value === "pending") {
+    status.value = "active";
+  } else {
+    status.value = "active";
+  }
 };
 </script>
 
