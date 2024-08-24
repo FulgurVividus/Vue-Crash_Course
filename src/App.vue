ce202bbd2be05316330ee96c3d24bcdbd295d7b4
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-24 19:49:43 +0500

    chore: ref() & Reactive values

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <script>
+import { ref } from "vue";
+
 export default {
-  data() {
-    return {
-      name: "John Doe",
-      status: "active",
-      tasks: ["Task 1", "Task 2", "Task 3"],
-      link: "https://www.google.com",
-    };
-  },
-  methods: {
-    toggleStatus() {
-      if (this.status === "active") {
-        this.status = "pending";
-      } else if (this.status === "pending") {
-        this.status = "active";
+  setup() {
+    const name = ref("John Doe");
+    const status = ref("active");
+    const tasks = ref(["Task 1", "Task 2", "Task 3"]);
+
+    const toggleStatus = () => {
+      if (status.value === "active") {
+        status.value = "pending";
+      } else if (status.value === "pending") {
+        status.value = "active";
       } else {
-        this.status = "active";
+        status.value = "active";
       }
-    },
+    };
+
+    return { name, status, tasks, toggleStatus };
   },
 };
 </script>
@@ -32,8 +32,7 @@ export default {
   <ul>
     <li v-for="task in tasks" :key="task">{{ task }}</li>
   </ul>
-  <!-- <a v-bind:href="link">Click for google</a> -->
-  <a :href="link">Click for google</a>
+
   <br />
 
   <!-- <button v-on:click="toggleStatus">Change status</button> -->
