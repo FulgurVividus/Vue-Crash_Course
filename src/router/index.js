e1fbd196309d5b7e1613a2272175502c0667ec0d
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-08-25 14:58:06 +0500

    feat: Vue Router and Home View

---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [{ path: "/", name: "Home", component: HomeView }],
+});
+
+export default router;
